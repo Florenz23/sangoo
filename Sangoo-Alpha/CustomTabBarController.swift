7314ee38a64f93c7bbafe34ac55db79aebf78d44
lama23 <florenz.erstling@lalalama.de>
2016-12-16 11:37:48 +0100

    initial commit

---
+++ Sangoo-Alpha/CustomTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTabBarController.swift
+//  Sangoo-Alpha
+//
+//  Created by Florenz Erstling on 16.12.16.
+//  Copyright © 2016 Florenz. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = []
+    }
+}
